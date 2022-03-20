230ee9c0ed091a7d8e5bf88ae2ccd3aa4d218b5a
César Castro <cmagarinho@gmail.com>
2022-03-20 04:46:16 +0000

    now accounting with multiple pages

--- BS4/scrapper.py
+++ BS4/scrapper.py
@@ -24,40 +24,77 @@ def get_product(productRAW):
       product['negotiable'] = isNegotiable
       product['date'] = date.encode('utf-8')
       product['url'] = productRAW.find('a')['href']
-      product['img'] = productRAW.find('img')['src']
+      product['img'] = productRAW.find('img')['src'] if productRAW.find('img') != None else 'NO IMAGE'
       product['location'] = location.encode('utf-8')
       return product
 
+def search_page(search,index = None):
+    URL = 'https://www.olx.pt/ads/q-' + search + "/"
+    if index != None:
+        URL = URL + '?page=' + str(index)
+    r = requests.get(URL)
+    print(URL)
+    soup = BeautifulSoup(r.content, 'html5lib')
+    return soup
 
 # 
 #----------------------- MAIN -------------------
 # 
 
-URL = "https://www.olx.pt/ads/q-rtx-2070/"
-r = requests.get(URL)
+search = raw_input('Enter your search:')
 
- # If this line causes an error, run 'pip install html5lib' or install html5lib
-soup = BeautifulSoup(r.content, 'html5lib')
-# print(soup.prettify())
+soup = search_page(search)
 
-numberOfProducts = 0;
+numberOfProducts = soup.find('div', attrs = {'class':'hasPromoted section clr'}).find("p").text.split(" ")[1];
+numberOfPages = int(soup.find('div', attrs = {'class':'pager rel clr'}).findAll('span', attrs = {'class': None})[-2].getText());
+currentPage = int(soup.find('span', attrs = {'class': 'block br3 c41 large tdnone lheight24 current'}).find('span').text)
+print(numberOfProducts) 
+print("Pages: " + str(numberOfPages))
+
 products = []
 
 productList = soup.find('table', attrs = {'id':'offers_table'})
-numberOfProducts = soup.find('div', attrs = {'class':'hasPromoted section clr'}).find("p").text.split(" ")[1]
 
-print(numberOfProducts) 
-
+# Cycle all the adds and builds the product objects
 offer = productList.find('div', attrs = {'class':'offer-wrapper'})
 products.append(get_product(offer))
 
-lastProduct = False
-while lastProduct == False:
+isLastProduct = False
+while isLastProduct == False:
     offer = offer.findNext('div', attrs = {'class':'offer-wrapper'})
     if offer == None:
-        lastProduct = True
+        isLastProduct = True
     else:
         products.append(get_product(offer))
+# Cycle all the adds and builds the product objects
+
+print("number of pages: " + str(numberOfPages) + "      page: " + str(1) + "       last product: " + products[len(products) -1]['name'])
+
+for x in range(2, numberOfPages + 1):
+        soup = search_page(search,x)
+        productList = soup.find('table', attrs = {'id':'offers_table'})
+
+        # Cycle all the adds and builds the product objects
+        offer = productList.find('div', attrs = {'class':'offer-wrapper'})
+        products.append(get_product(offer))
+
+        isLastProduct = False
+        while isLastProduct == False:
+            offer = offer.findNext('div', attrs = {'class':'offer-wrapper'})
+            if offer == None:
+                isLastProduct = True
+            else:
+                products.append(get_product(offer))
+        # Cycle all the adds and builds the product objects
+
+        print("number of pages: " + str(numberOfPages) + "      page: " + str(x) + "       last product: " + products[len(products) -1]['name'])
+        if(x == numberOfPages):
+            break
+        else:
+            x = x + 1
+        
+
+
 
 filename = 'products.csv'
 with open(filename, 'wb') as f:
